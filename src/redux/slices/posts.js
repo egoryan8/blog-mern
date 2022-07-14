2f09e3b7cce5369d8d1e154e1b7086eeebd13cad
Fogel Egor <fogel_92@bk.ru>
2022-07-14 19:43:29 +0700

    feat: posts and tags from backend

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const { data } = await axios.get('/posts');
+  return data;
+});
+
+export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
+  const { data } = await axios.get('/tags');
+  return data;
+});
 
 const initialState = {
   posts: {
@@ -14,7 +25,33 @@ const initialState = {
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducer: {},
+  reducers: {},
+  extraReducers: {
+    [fetchPosts.pending]: (state) => {
+      state.posts.items = [];
+      state.posts.status = 'loading';
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.posts.items = action.payload;
+      state.posts.status = 'loaded';
+    },
+    [fetchPosts.rejected]: (state) => {
+      state.posts.items = [];
+      state.posts.status = 'error';
+    },
+    [fetchTags.pending]: (state) => {
+      state.tags.items = [];
+      state.tags.status = 'loading';
+    },
+    [fetchTags.fulfilled]: (state, action) => {
+      state.tags.items = action.payload;
+      state.tags.status = 'loaded';
+    },
+    [fetchTags.rejected]: (state) => {
+      state.tags.items = [];
+      state.tags.status = 'error';
+    },
+  },
 });
 
 export const postsReducer = postsSlice.reducer;
